6e0202a3d00c055f24ef62ea99d2f1d365fbc0d7
Spyros Mouchlianitis <spirosgsaaa@gmail.com>
2025-09-26 15:50:01 +0300

    feat: postgres integration

--- db/db.go
+++ db/db.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"log/slog"
 
+	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
 
 var Conn *gorm.DB
@@ -22,6 +24,47 @@ func Connect(models ...interface{}) error {
 	err = Conn.AutoMigrate(models...)
 	if err != nil {
 		return fmt.Errorf("failed to migrate models err=%v", err)
+	}
+	return nil
+}
+
+// Init initializes the database connection and runs migrations.
+func Init(dsn, dbSchema string, models ...interface{}) error {
+	var err error
+	Conn, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix:   dbSchema + ".",
+			SingularTable: false,
+		},
+	})
+	if err != nil {
+		return fmt.Errorf("unable to connect to database, %w", err)
+	}
+
+	err = createSchemaIfNotExists(dbSchema)
+	if err != nil {
+		return err
+	}
+
+	// Run migrations
+	err = Conn.AutoMigrate(models...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func createSchemaIfNotExists(dbSchema string) error {
+	var count int
+	Conn.Raw("SELECT COUNT(*) FROM pg_catalog.pg_namespace WHERE nspname = ?", dbSchema).Scan(&count)
+
+	if count > 0 {
+		return nil
+	}
+
+	err := Conn.Exec(fmt.Sprintf("CREATE SCHEMA %s", dbSchema)).Error
+	if err != nil {
+		return fmt.Errorf("failed to create schema=%s, err=%v", dbSchema, err)
 	}
 	return nil
 }
