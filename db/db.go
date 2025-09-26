d66603448e9406b143a69e0e3ffc790f543dd7d7
Spyros Mouchlianitis <spirosgsaaa@gmail.com>
2025-09-26 15:50:30 +0300

    rm unused method

--- db/db.go
+++ db/db.go
@@ -2,31 +2,13 @@ package db
 
 import (
 	"fmt"
-	"log/slog"
 
 	"gorm.io/driver/postgres"
-	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
 )
 
 var Conn *gorm.DB
-
-func Connect(models ...interface{}) error {
-	var err error
-	Conn, err = gorm.Open(sqlite.Open("./db.sqlite"), &gorm.Config{})
-	if err != nil {
-		return fmt.Errorf("unable to connect to database, err=%w", err)
-	}
-
-	slog.Info("connected to database")
-
-	err = Conn.AutoMigrate(models...)
-	if err != nil {
-		return fmt.Errorf("failed to migrate models err=%v", err)
-	}
-	return nil
-}
 
 // Init initializes the database connection and runs migrations.
 func Init(dsn, dbSchema string, models ...interface{}) error {
