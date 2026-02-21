c817f3f53e890c053be4fd8646028d781de1d309
Spyros Mouchlianitis <spirosgsaaa@gmail.com>
2026-02-21 19:16:59 +0200

    formatting

--- env/env.go
+++ env/env.go
@@ -1,27 +1,27 @@
 package env
 
 import (
-    "log/slog"
-    "os"
+	"log/slog"
+	"os"
 
-    "github.com/SpyrosMoux/godotenv"
+	"github.com/SpyrosMoux/godotenv"
 )
 
 func LoadEnvVariable(variable string) string {
-    if err := godotenv.Load(); err != nil {
-        slog.Warn("no .env file found, attempting to read variables from host environment")
-    }
+	if err := godotenv.Load(); err != nil {
+		slog.Warn("no .env file found, attempting to read variables from host environment")
+	}
 
-    variableValue := getEnvOrExit(variable)
+	variableValue := getEnvOrExit(variable)
 
-    return variableValue
+	return variableValue
 }
 
 func getEnvOrExit(key string) string {
-    value := os.Getenv(key)
-    if value == "" {
-        slog.Error("environment variable not set", "variable", key)
-        os.Exit(1)
-    }
-    return value
+	value := os.Getenv(key)
+	if value == "" {
+		slog.Error("environment variable not set", "variable", key)
+		os.Exit(1)
+	}
+	return value
 }
